f90cf9734efb9b0af72f58d804df7cf8c307d25d
James <jameshowie17@outlook.com>
2023-10-13 13:09:16 +0100

    added onDelete function in chapter-actions.tsx to allow for a chapter the chapter to be deleted. added handling to delete the video from the muxdata database where the users videos are stored and handling for if the course has no chapters to unpublish it all together

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-actions.tsx
@@ -2,7 +2,11 @@
 
 import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface ChapterActionsProps {
   disabled: boolean;
@@ -17,17 +21,34 @@ export const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+      toast.success("Chapter deleted");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="flex item-center gap-x-2">
       <Button
         onClick={() => {}}
-        disabled={disabled}
+        disabled={disabled || isLoading}
         variant="outline"
         size="sm"
       >
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
-      <ConfirmModal onConfirm={() => {}}>
+      <ConfirmModal onConfirm={onDelete}>
         <Button className="sm">
           <Trash className="h-4 w-4" />
         </Button>
