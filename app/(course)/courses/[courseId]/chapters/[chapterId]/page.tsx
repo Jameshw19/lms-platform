8028614e73e0a0144622aebaa1ef989db3b33519
James <jameshowie17@outlook.com>
2023-10-17 12:02:59 +0100

    added video player to the course chapter id page to show the user the video for that chapter

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,6 +1,8 @@
 import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { VideoPlayer } from "./_components/video-player";
 
 const ChapterIdPage = async ({
   params,
@@ -37,7 +39,32 @@ const ChapterIdPage = async ({
   const isLocked = !chapter.isFree && !purchased;
   const completeOnEnd = !!purchased && !userProgress?.isCompleted; //if we have a purchase and userProgress is complete
 
-  return <div>chapter id</div>;
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner label="You already completed this chapter." variant="success" />
+      )}
+      {isLocked && (
+        <Banner
+          label="You need to purchase this course to watch this chapter."
+          variant="warning"
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4 ">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChapterIdPage;
