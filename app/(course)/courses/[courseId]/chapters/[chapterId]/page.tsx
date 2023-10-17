14db0e2f798b142fde07b49db3511eea812982d4
James <jameshowie17@outlook.com>
2023-10-17 11:48:08 +0100

    get-chapter.ts actions; getting the information and handling cases that will be used in the chapterid page where we have added props aswell as handling errors

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,7 +1,43 @@
-const ChapterIdPage = () => {
-    return ( <div>
-        chapter id
-    </div> );
-}
- 
+import { getChapter } from "@/actions/get-chapter";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchased,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchased;
+  const completeOnEnd = !!purchased && !userProgress?.isCompleted; //if we have a purchase and userProgress is complete
+
+  return <div>chapter id</div>;
+};
+
 export default ChapterIdPage;
