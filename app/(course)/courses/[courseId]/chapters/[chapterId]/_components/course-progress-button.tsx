fe3508aa7e44913127454f44613d09063e421acf
James <jameshowie17@outlook.com>
2023-10-18 19:04:30 +0100

    added course progress button to the course chapter page to allow user to click the button if they have finished that chapter of the course

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CheckCircle, XCircle } from "lucide-react";
+
+interface CourseProgressButtonProps {
+  chapterId: string;
+  courseId: string;
+  isCompleted?: boolean;
+  nextChapterId?: string;
+}
+
+export const CourseProgressButton = ({
+  chapterId,
+  courseId,
+  isCompleted,
+  nextChapterId,
+}: CourseProgressButtonProps) => {
+  const Icon = isCompleted ? XCircle : CheckCircle;
+
+  return (
+    <Button type="button" variant={isCompleted ? "outline" : "success"} className="w-full md:w-auto">
+      {isCompleted ? "Not completed" : "Mark as complete"}
+      <Icon className="h-4 w-4 ml-2" />
+    </Button>
+  );
+};
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -7,6 +7,7 @@ import { CourseEnrollButton } from "./_components/course-enroll-button";
 import { Separator } from "@/components/ui/separator";
 import { Preview } from "@/components/preview";
 import { File } from "lucide-react";
+import { CourseProgressButton } from "./_components/course-progress-button";
 
 const ChapterIdPage = async ({
   params,
@@ -70,7 +71,14 @@ const ChapterIdPage = async ({
           <div className="p-4 flex flex-col md:flex-row items-center justify-between">
             <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
             {purchased ? (
-              <div>{/* / TODO: Add courseprogress button */}</div>
+              <div>
+                <CourseProgressButton
+                  chapterId={params.chapterId}
+                  courseId={params.courseId}
+                  nextChapterId={nextChapter?.id}
+                  isCompleted={!!userProgress?.isCompleted}
+                />
+              </div>
             ) : (
               <CourseEnrollButton
                 courseId={params.courseId}
